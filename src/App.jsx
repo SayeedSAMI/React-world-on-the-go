989d580a69ebd9ffe7a67076d2176429c520dc48
SayeedSami <sayeedsami26@gmail.com>
2026-04-09 00:01:15 +0600

    countries api fetching part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Suspense } from "react";
 import "./App.css";
 import Countries from "./components/countries/countries";
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all').then(res => res.json())
+
 
 function App() {
   return (
     <>
-      <Countries></Countries>
+      <Suspense fallback={<h1>Loading...</h1>} >
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
   );
 }
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { use } from "react";
 
-const Countries = () => {
+const Countries = ({countriesPromise}) => {
+  const countriesData = use(countriesPromise);
+  const  Content = countriesData.countries;
+  console.log(Content);
+  console.log(Content.length);
+
+//  console.log("FULL DATA:", countriesData);
+//  console.log("COUNTRIES LENGTH:", countriesData?.countries?.length);
+// console.log("COUNTRIES:", countriesData.countries);
+// console.log("STRINGIFIED:", JSON.stringify(countriesData));
+
   return (
     <div>
       <h1>React World on the go...</h1>
+      <h3>its not working</h3>
     </div>
   );
 };
